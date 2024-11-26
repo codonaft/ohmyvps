13312b3c03a54c1e399328d632a02b62a9f53583
Alexander Lopatin <alopatindev@codonaft.com>
2024-11-27 03:50:43 +0800

    Fix limit: use real IPs, not the Cloudflare proxy IPs

--- alpine/alpine-root/usr/local/sbin/update-cloudflare-ips.sh
+++ alpine/alpine-root/usr/local/sbin/update-cloudflare-ips.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
+PATH_PREFIX="/var/tmp/cloudflare-ipv"
+NGINX_CONF="/etc/nginx/cloudflare.conf"
+
+load () {
+  cat ${PATH_PREFIX}{4,6}.txt 2>>/dev/null
+}
+
 sum () {
-  cat /tmp/cloudflare-ipv{4,6}.txt 2>>/dev/null | sha256sum
+  load | sha256sum
 }
 
 download () {
   version="$1"
   cidr="$2"
-  output="/var/tmp/cloudflare-ipv${version}.txt"
+  output="${PATH_PREFIX}${version}.txt"
   temp="${output}.tmp"
   sudo -u nobody wget --timeout=30 --tries=10 -qO - "https://www.cloudflare.com/ips-v${version}" | grepcidr -e "${cidr}" | sort -u > "${temp}"
   [ "$(stat --format='%s' ${temp})" -gt 0 ] && mv "${temp}" "${output}"
@@ -18,5 +25,8 @@ download 4 '0.0.0.0/0'
 download 6 '::/0'
 after=$(sum)
 
-[ "${before}" != "${after}" ]
-exit $?
+[ "${before}" != "${after}" ] && {
+  temp="${NGINX_CONF}.tmp"
+  load | sed 's!^!set_real_ip_from !;s!$!;!' > "${temp}"
+  mv "${temp}" "${NGINX_CONF}"
+} || exit 1
