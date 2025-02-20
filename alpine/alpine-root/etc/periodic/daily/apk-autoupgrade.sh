c7d451fea4f5e50306f9956d39de2ccc3b48045f
Alexander Lopatin <alopatindev@codonaft.com>
2025-02-20 12:00:50 +0800

    Update Alpine version

--- alpine/alpine-root/etc/periodic/daily/apk-autoupgrade.sh
+++ alpine/alpine-root/etc/periodic/daily/apk-autoupgrade.sh
@@ -3,8 +3,9 @@
 logger 'apk-autoupgrade'
 
 eject || :
-alpine_version=$(grep -oE '[0-9]+\.[0-9]+' /etc/alpine-release)
-setup-apkrepos https://dl-cdn.alpinelinux.org/alpine/v${alpine_version}/{main,community} || :
+setup-apkrepos https://dl-cdn.alpinelinux.org/alpine/latest-stable/{main,community} || :
+apk add --no-cache --upgrade apk-tools 2>>/dev/stdout | logger || :
 apk upgrade --no-cache 2>>/dev/stdout | logger
 apk del doas linux-lts openssh-server-pam syslinux tiny-cloud-alpine tiny-cloud || :
+rm -f /etc/motd
 # TODO: detect nginx and sshd update, restart them
